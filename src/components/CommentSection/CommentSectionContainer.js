27a4a6555bea86258bf4ededd382616eba5a66cc
jleon1992 <36936276+jleon1992@users.noreply.github.com>
2020-06-09 18:36:34 -0400

    added the comment section

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,11 +6,19 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  // const {postId, comments} = props
+  const [comments, changeComment] = useState(props.comments)
+  console.log(comments)
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {
+        comments.map(comment=>{
+          return <Comment comment = {comment} />
+        })
+      }
+      <CommentInput  />
     </div>
   );
 };
